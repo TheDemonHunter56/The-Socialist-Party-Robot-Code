ca4ff060ba0ba189bc199d9c390133b509739fd5
KysonNg <grimghostthen@gmail.com>
2025-02-06 16:41:53 -0500

    Done

--- src/main/java/robot/intake/Intake.java
+++ src/main/java/robot/intake/Intake.java
@@ -1,5 +1,10 @@
 package robot.intake;
 
-public class Intake {
-    
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Intake extends SubsystemBase{
+    private final CANSparkMax Roll = new CANSparkMax(Ports.Intake.ROLL, MotorType.kBrushless);
 }
