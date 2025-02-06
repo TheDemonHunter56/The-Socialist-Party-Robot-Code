81d40feec7a60b2036a221e601bf6f6edf2f1b0e
TheDemonHunter56 <99517263+TheDemonHunter56@users.noreply.github.com>
2025-02-06 21:40:51 +0000

    .

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -9,5 +9,11 @@ public final class Ports {
   public static final class Drive {
     public static final int RIGHT_LEADER = 2;
     public static final int RIGHT_FOLLOWER = 3;
+    public static final int LEFT_LEADER = 4;
+    public static final int LEFT_FOLLOWER = 5;
+  }
+
+  public static final class Intake {
+    public static final int ROLL = 6;
   }
 }
