7989e8ae5e554c86353d94a5c03c6da3e370da45
KysonNg <grimghostthen@gmail.com>
2025-03-13 20:03:39 +0000

    Commandline needed

--- src/main/java/robot/intake/Intake.java
+++ src/main/java/robot/intake/Intake.java
@@ -1,11 +1,17 @@
 package robot.intake;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import robot.Ports;
-
-
-public class Intake extends SubsystemBase{
-    public static final CANSparkMax Roll = new CANSparkMax(Ports.Intake.ROLL, MotorType.kBrushless);
+public class Intake extends CommandBase{
+    private final Intake intake;
+        
+        public Intake(Intake intake) {
+            this.intakeMotor = intakeMotor;
+    }
+    public void startMotor() {
+        intakeMotor.set(1);
+    }
+    public void stopMotor() {
+        intakeMotor.set(0);
+    }
 }
