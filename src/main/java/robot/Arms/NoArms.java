e67a5fd5d8877c3ca0fb05af143439b51075dee4
Nina Raycel Andrada <xenoneria@gmail.com>
2025-04-14 19:52:31 +0000

    worked on arm constants and added noarms

---
+++ src/main/java/robot/Arms/NoArms.java
@@ -0,0 +1,15 @@
+package robot.Arms;
+
+//*for when the arm is not useable*/
+public class NoArms implements ArmsIO {
+
+public double posiition() {
+    return 0;
+}
+
+public double velocity() {
+    return 0;
+}
+
+public void setVoltage(0) {}
+}
---
+++ tants.java NoArms.java
@@ -0,0 +1,48 @@
+[33mcommit 7d3bd1a8d80187bd826b4eafec9cd9c95ad39ad2[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mArms[m[33m, [m[1;31morigin/Arms[m[33m)[m
+Author: Nina Raycel Andrada <xenoneria@gmail.com>
+Date:   Fri Mar 14 00:08:16 2025 +0000
+
+    slight changes
+
+[33mcommit e7d85b4f71c5f57afe7e93382187b82d109f415d[m
+Author: Nina Raycel Andrada <xenoneria@gmail.com>
+Date:   Fri Mar 14 00:05:36 2025 +0000
+
+    deleted noarms
+
+[33mcommit 4804d28de11924e80e6e5258d4c8aa9134668cca[m
+Merge: b62b60e 125fe38
+Author: Nina Raycel Andrada <xenoneria@gmail.com>
+Date:   Thu Feb 13 21:39:20 2025 +0000
+
+    Merge remote-tracking branch 'refs/remotes/origin/Arms' into Arms
+
+[33mcommit b62b60e0496c4dddaa3a56934893b62e2a0f9047[m
+Author: Nina Raycel Andrada <xenoneria@gmail.com>
+Date:   Thu Feb 13 21:38:49 2025 +0000
+
+    Synced
+
+[33mcommit 61b110ec22190cb84916d097dbf32df57bbeef6a[m
+Author: Nina Raycel Andrada <xenoneria@gmail.com>
+Date:   Thu Feb 13 21:35:28 2025 +0000
+
+    demo changes
+
+[33mcommit 125fe38c8ddb4bffce98999c3913c22af7e4529f[m
+Author: Gurgle <jianga3@bxscience.edu>
+Date:   Thu Feb 13 21:30:12 2025 +0000
+
+    Added some base file required. For syncing
+
+[33mcommit 0aba56b24809425923caa3cba5345654d03761d9[m
+Author: Gurgle <jianga3@bxscience.edu>
+Date:   Mon Feb 10 22:53:24 2025 +0000
+
+    Added some variables need for Arm class and added method that set them to facotry seeting.
+
+[33mcommit 3e952a66220b55650bc1a159fd369a6c10d6e985[m
+Author: Gurgle <jianga3@bxscience.edu>
+Date:   Mon Feb 10 21:26:00 2025 +0000
+
+    Added Arms folder
