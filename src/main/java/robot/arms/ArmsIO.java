87dc1af155cd38a4c4bdee53c5a74263e3b511d2
Gurgle <jianga3@bxscience.edu>
2025-04-24 20:45:30 +0000

    Fixed ArmsIO and RealArms

--- src/main/java/robot/arms/ArmsIO.java
+++ src/main/java/robot/arms/ArmsIO.java
@@ -3,23 +3,12 @@ package robot.arms;
 public interface ArmsIO {
     //*Returns the angle of the arm in degrees */
     public double currentAngle();
-    /**
-     * Stops the arms from moving 
-    */
-    public void stopArms();
-    /** 
-     * resets arm to starting poisiton */
-    public void reset();
 
     /**
      * starts clamping the coral*/
     public void setClampPower(double power);
      /**
-     * Moves the arm into position in degrees
+     * Sets the voltage of the motors
     */
-    public void moveTargetDegrees(double DegreesFromTarget);
-    /**
-     * stops clamping the coral
-    */
-    public void stopClamp();
+    public void setVoltage(double Voltage);
 }
