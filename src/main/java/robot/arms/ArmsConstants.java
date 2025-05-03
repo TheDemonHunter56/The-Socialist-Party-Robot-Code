21cc4d5f16051e6242adcef2317f48805c16f9b3
Nina Raycel Andrada <xenoneria@gmail.com>
2025-05-03 00:39:35 +0000

    adding changes

--- src/main/java/robot/arms/ArmsConstants.java
+++ src/main/java/robot/arms/ArmsConstants.java
@@ -2,22 +2,12 @@
 
 package robot.arms;
 
-import com.pathplanner.lib.util.PIDConstants;
-
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.trajectory.ExponentialProfile.Constraints;
 import edu.wpi.first.units.Angle;
-import edu.wpi.first.units.Distance;
-import edu.wpi.first.units.Mass;
 import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.Mult;
-import edu.wpi.first.units.Unit;
 import edu.wpi.first.units.Velocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import robot.Constants;
 import edu.wpi.first.units.Units;
 
-@SuppressWarnings("unused")
 public class ArmsConstants extends SubsystemBase{
 
   public static final double THROUGHBORE_GEARING = 16.0 / 54.0;
@@ -29,11 +19,10 @@ public static final Measure<Angle> STARTING_ANGLE = Units.Degrees.of(10);
   public static final Measure<Angle> MIN_ANGLE = Units.Degrees.of(0);
   public static final Measure<Angle> MAX_ANGLE = Units.Degrees.of(180);
 
-public static final double DEFAULT_SPEED = 1;
-public static final double STILL_SPEED = 0;
+  
+
 public static final double MAX_SPEED = 2;
-public static final double DEFAULT_TORQUE = 0;
-public static final double MAX_TORQUE = 1;
+
 
 public static final double STARTING_VOLTAGE = 0;
 public static final double MAX_VOLTAGE = 12;
--- src/main/java/robot/arms/NoArms.java
+++ src/main/java/robot/arms/NoArms.java
@@ -2,31 +2,17 @@ package robot.arms;
 
 public class NoArms implements ArmsIO {
 
-    public void ArmStart() {
-        // No arms to start
+    @Override
+    public void currentAngle() {;
     }
 
-    public void reset() {
-        // No arms to reset
-    }
-
-    public double currentAngle() {
-        return 0; // No angle to return
-    }
-
-    public void stopArms() {
-        // No arms to stop
-    }
-
+    @Override
     public void setClampPower(double power) {
-        // No clamp to set power for
     }
 
-    public void stopClamp() {
-        // No clamp to stop
+    @Override
+    public void setVoltage(double Voltage) {
     }
 
-    public void moveTargetDegrees(double targetDegrees) {
-        // No target degrees to move to
-    }
+    
 }
