fdd6849031ae62f82ce159017bed63f618e7eaa7
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2025-05-13 08:26:58 -0400

    Made some contingencies for controller error

--- src/main/java/robot/drive/Drive.java
+++ src/main/java/robot/drive/Drive.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import robot.Ports;
+import math;
 
 public class Drive extends SubsystemBase{
     private final CANSparkMax leftLeader = new CANSparkMax(Ports.Drive.LEFT_LEADER, MotorType.kBrushless);
@@ -29,8 +30,30 @@ public class Drive extends SubsystemBase{
   }
     //Applies voltage to the motors, uses percentages with range -1 < X < 1
     private void drive(double leftSpeed, double rightSpeed) {
-        leftLeader.set(leftSpeed);
-        rightLeader.set(rightSpeed);
+        //If someone puts in a speed that is greater than or less than 1/-1 for both speeds
+        if (leftSpeed > 1) leftSpeed = 1;
+        if (leftSpeed < -1) leftSpeed = -1;
+        if (rightSpeed > 1) rightSpeed = 1;
+        if (rightSpeed < -1) rightSpeed = -1;
+        //If the joystick is in the middle but doesn't give the value of exactly 0
+        if (Math.abs(leftSpeed) > 0.1) && (Math.abs(rightSpeed) > 0.1) {
+            leftLeader.set(leftSpeed);
+            rightLeader.set(rightSpeed);
+        } else {
+            stop()
+        }
+
+    //stops the robot from moving
+    public void stop() {
+        leftLeader.set(0);
+        rightLeader.set(0);
+    }
+
+    //returns (leftSpeed, rightSpeed)
+    public double getSpeed() {
+        return (leftLeader.get(), rightLeader.get())
+    }
+            
   }
     //actual method that will return the command
     public Command drive(DoubleSupplier vLeft, DoubleSupplier vRight){
